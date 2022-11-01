93190222d9a58c3290bc65cd364bd7dc96189d33
Shashank Singh <52758288+shashank3107@users.noreply.github.com>
2022-11-01 21:38:41 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:facebook_clone/views/profile_page_view.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    home: ProfilePageView(),
+  ));
+}
