4dca649ffc31bcbbe3cdb16bbadf79b044598b36
Shashank Singh <52758288+shashank3107@users.noreply.github.com>
2022-11-01 23:04:44 +0530

    added Profile Header UI

--- lib/views/profile_page_view.dart
+++ lib/views/profile_page_view.dart
@@ -8,8 +8,93 @@ class ProfilePageView extends StatefulWidget {
 }
 
 class _ProfilePageViewState extends State<ProfilePageView> {
+  var screenHeight;
+  var screenWidth;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: ListView(children: [profileHeader()]),
+    );
+  }
+
+  Widget profileHeader() {
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Container(
+              height: screenHeight / 4,
+              color: Colors.grey,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.camera_alt_outlined),
+                  Text(' Add Cover Photo'),
+                ],
+              ),
+            ),
+            Container(
+              height: 130,
+              color: Colors.white,
+            ),
+          ],
+        ),
+        Positioned(
+          top: screenHeight / 4 - 60,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              //mainAxisSize: MainAxisSize.max,
+              children: [
+                CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    maxRadius: 50,
+                    child: SizedBox(
+                        width: 120,
+                        height: 120,
+                        child: ClipOval(
+                          child: Image.asset(
+                            'assets/cat_pic.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                        ))),
+                const SizedBox(
+                  width: 5,
+                ),
+                const Text(
+                  'Shashank Singh',
+                  style: TextStyle(fontSize: 22),
+                ),
+                Row(
+                  children: [
+                    rectangularButton(Colors.blue, 'Add to story'),
+                    IconButton(
+                        onPressed: (() {}), icon: const Icon(Icons.more_horiz))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget rectangularButton(Color color, String buttonText) {
+    return MaterialButton(
+      minWidth: screenWidth - 60,
+      color: color,
+      onPressed: (() {
+        debugPrint('Button Tapped !');
+      }),
+      child: Row(
+        children: [const Icon(Icons.add_circle), Text(buttonText)],
+      ),
+    );
   }
 }
